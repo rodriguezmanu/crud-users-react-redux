452ce059ee270b4850896389b55e05b3b13d6021
Manuel Rodriguez <rodriguez.manu@gmail.com>
2018-09-05 13:37:49 -0300

    Adding filter users

--- src/containers/users/Users.js
+++ src/containers/users/Users.js
@@ -1,20 +1,82 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { getUsers, deleteUser, filterUser } from '../../actions/users.actions';
 
 export class Users extends React.PureComponent {
   static propTypes = {
-    prop: PropTypes,
+    users: PropTypes.shape([]).isRequired,
+    user: PropTypes.shape({}).isRequired,
+  };
+
+  componentWillMount() {
+    const { getUsers } = this.props;
+    getUsers();
+  }
+
+  /**
+   * Delete user
+   * @params {Number} id
+   */
+  deleteUserHandler = id => {
+    const { user, deleteUser } = this.props;
+
+    deleteUser(id, user.data.id === id);
+  };
+
+  /**
+   * Filter user list action
+   */
+  filterUserList = event => {
+    const { filterUser } = this.props;
+    filterUser(event.target.value);
   };
 
   render() {
-    return <div>Users</div>;
+    const { users } = this.props;
+
+    return (
+      <div>
+        <div>
+          <input type="text" placeholder="Search" onChange={this.filterUserList} />
+        </div>
+        <ul>
+          {users.filtered &&
+            users.filtered.map(item => (
+              <div key={item.id}>
+                <li>
+                  <p>{item.name}</p>
+                  <p>{item.email}</p>
+                  <p>{item.role}</p>
+                </li>
+                <div>
+                  <button
+                    type="button"
+                    onClick={e => {
+                      this.deleteUserHandler(item.id);
+                    }}
+                  >
+                    Delete User
+                  </button>
+                </div>
+              </div>
+            ))}
+        </ul>
+      </div>
+    );
   }
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+  users: state.users,
+  user: state.user,
+});
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = {
+  getUsers,
+  deleteUser,
+  filterUser,
+};
 
 export default connect(
   mapStateToProps,
--- src/reducers/users.reducer.js
+++ src/reducers/users.reducer.js
@@ -1,4 +1,9 @@
-import { GET_USERS_REQUEST, GET_USERS_SUCCESS, GET_USERS_FAILURE } from '../constants/actionTypes';
+import {
+  GET_USERS_REQUEST,
+  GET_USERS_SUCCESS,
+  GET_USERS_FAILURE,
+  FILTER_USER_SUCCESS,
+} from '../constants/actionTypes';
 
 const initialState = {
   isFetching: false,
@@ -15,12 +20,21 @@ const users = (state = initialState, action) => {
       return {
         ...state,
         data: action.data,
+        filtered: action.data,
         isFetching: false,
       };
     case GET_USERS_FAILURE:
       return {
         ...state,
         error: action.data,
+        isFetching: false,
+      };
+    case FILTER_USER_SUCCESS:
+      return {
+        ...state,
+        filtered: state.data.filter(val =>
+          val.name.toLowerCase().includes(action.name.toLowerCase())
+        ),
         isFetching: false,
       };
     default:
